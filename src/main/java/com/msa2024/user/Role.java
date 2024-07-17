a2f9a566a24124b9be391e7f328c1182b08dd123
SungJun <SungJun@DESKTOP-SUNGJUN>
2024-07-17 10:16:08 +0900

    중간저장

---
+++ src/main/java/com/msa2024/user/Role.java
@@ -0,0 +1,6 @@
+package com.msa2024.user;
+
+public enum Role {
+  ADMIN,
+  USER
+}
---
+++ src/main/java/com/msa2024/user/UserManager.java
@@ -0,0 +1,45 @@
+package com.msa2024.user;
+
+import java.util.Scanner;
+
+public class UserManager {
+  
+  
+  
+  public void signUp() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\n=================================================================\n");
+    System.out.println("[INFO] 회원가입을 위해 아래의 양식을 채워주세요.\n");
+    
+    System.out.print("* 이메일 : ");
+    String email = sc.next();  
+    System.out.print("* 이름 : ");
+    String name = sc.next();   
+    System.out.print("* 비밀번호 : ");
+    String password = sc.next();   
+    
+    
+    System.out.println("회원가입이 완료되었습니다.");
+    return;
+  }
+  
+  
+  public void signIn() {
+  
+    Scanner sc = new Scanner(System.in);
+    System.out.print("* 이메일 : ");
+    String email = sc.next();  
+    System.out.print("* 비밀번호 : ");
+    String password = sc.next();   
+    
+    
+    System.out.println("로그인 되었습니다!!");
+    
+  }
+  
+  
+  
+  
+  
+
+}
