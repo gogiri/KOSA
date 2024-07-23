e5c03c3a59aeef46d10d58d94effd981efe7cf6a
bizbaeja <bjgbjg112@gmail.com>
2024-07-23 09:32:25 +0900

    재시도

---
+++ src/main/java/com/msa2024/util/FileUtil.java
@@ -0,0 +1,19 @@
+//package com.msa2024.util;
+//
+//import com.fasterxml.jackson.core.type.TypeReference;
+//import com.google.gson.reflect.TypeToken;
+//
+//import java.util.List;
+//
+//public interface FileUtil<T> {
+//    void writeToFile(String filename, List<T> items);
+//    List<T> readFromFile(String filename);
+//    List<T> readFromFile(String filename, TypeToken<List<T>> typeToken);
+//
+//    List<T> readFromFile(String filename, TypeReference<List<T>> typeReference);
+//
+//    void writeToFileWithJackson(String filename, List<T> items);
+//    List<T> readFromFileWithJackson(String filename, Class<T> clazz);
+//
+//    List<T> readFromFileWithJackson(String filename, TypeReference<List<T>> typeReference);
+//}
